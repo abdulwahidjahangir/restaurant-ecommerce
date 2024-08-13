81913adf3ab95658230935d91bfa9f7e328dade3
Abdul Wahid Jahangir <abdulwahidjahangir12@outlook.com>
2024-08-13 14:49:24 +0200

    updating order status compoenent

--- components/OrderStatus.tsx
+++ components/OrderStatus.tsx
@@ -1,36 +1,60 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
+import classNames from "classnames";
 
 interface StatusRowProps {
   preparationTime: string;
 }
 
 const OrderStatus: React.FC<StatusRowProps> = ({ preparationTime }) => {
-  const currentTime = new Date();
+  const [status, setStatus] = useState<string>("Not prepared yet");
 
-  const elapsedMinutes =
-    (currentTime.getTime() - new Date(preparationTime).getTime()) / (1000 * 60);
+  useEffect(() => {
+    const updateStatus = () => {
+      const currentTime = new Date();
+      const preparationDate = new Date(preparationTime);
 
-  const status =
-    elapsedMinutes > 35
-      ? "Delivered"
-      : elapsedMinutes > 15
-      ? "On the way to deliver"
-      : elapsedMinutes > 10
-      ? "Prepared"
-      : "Not prepared yet";
+      const elapsedMinutes =
+        (currentTime.getTime() - preparationDate.getTime()) / (1000 * 60);
+
+      const newStatus =
+        elapsedMinutes > 35
+          ? "Delivered"
+          : elapsedMinutes > 15
+          ? "On the way to deliver"
+          : elapsedMinutes > 10
+          ? "Prepared"
+          : "Not prepared yet";
+
+      setStatus(newStatus);
+    };
+
+    // Initial status calculation
+    updateStatus();
+
+    // Interval to update status every minute
+    const interval = setInterval(updateStatus, 60000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(interval);
+  }, [preparationTime]);
 
   return (
     <td
-      className={`py-6 px-1 ${status === "Prepared" && "bg-yellow-300"} ${
-        status === "Delivered" && "bg-blue-300"
-      } ${status === "On the way to deliver" && "bg-green-300"}`}
+      className={classNames(
+        "py-6 px-1",
+        {
+          "bg-yellow-300": status === "Prepared",
+          "bg-blue-300": status === "Delivered",
+          "bg-green-300": status === "On the way to deliver",
+        }
+      )}
     >
       {status === "Prepared" && "Prepared"}
       {status === "On the way to deliver" && "On the Way (approx. 15min)"}
       {status === "Delivered" && "Delivered"}
-      {status === "Not prepared yet" && "Order Recieved"}
+      {status === "Not prepared yet" && "Order Received"}
     </td>
   );
 };
